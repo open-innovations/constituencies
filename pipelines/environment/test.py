629f38350e1cf058e6afc548c27d1c07b1af75c3
slowe <299787+slowe@users.noreply.github.com>
2023-08-22 13:58:10 +0100

    Remove try/except

--- pipelines/environment/test.py
+++ pipelines/environment/test.py
@@ -2,9 +2,11 @@
 import json
 from shapely.geometry import shape, Point
 from shapely.validation import make_valid
+
 lon = -1.063984
 lat = 52.11284
 point = Point(lon,lat)
+
 with open('src/_data/geojson/constituencies-2022.geojson') as f:
     geo = json.load(f)
 for feature in geo['features']:
@@ -18,13 +20,10 @@ for feature in geo['features']:
 for feature in geo['features']:
     b = feature['geometry']['bounds']
     if lon >= b[0] and lon <= b[2] and lat >= b[1] and lat <= b[3]:
-        try:
-            # It has passed the initial bounding-box test
-            # so check if it is really within the polygon(s)
-            if feature['geometry']['polygon'].contains(point):
-                print('Bounds:', feature['properties']['bounds'])
-                #print('yes', feature['properties']['PCON22NM'])
-                continue
-        except:
-            print('contains() is invalid',point,feature['geometry']['bounds'])
-            print(feature['geometry']['polygon'])
+        print(point,'is within bounds',b,'for',feature['properties']['PCON22NM'])
+        # It has passed the initial bounding-box test
+        # so check if it is really within the polygon(s)
+        if feature['geometry']['polygon'].contains(point):
+            print('\tA full check gives:', feature['properties']['PCON22NM'],feature['properties']['PCON22CD'])
+            #print('yes', feature['properties']['PCON22NM'])
+            continue
