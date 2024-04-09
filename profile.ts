39e25f8ea2cf96268c21d864a7f97e7a03f248bb
Giles Dring <giles@dringtech.com>
2024-04-09 15:36:38 +0100

    Adding debug / profiling launcher

---
+++ profile.ts
@@ -0,0 +1,21 @@
+console.log('Beginning startup delay...');
+
+await new Promise((resolve) => {
+  setTimeout(() => {
+    console.log('Startup delay completed...');
+    resolve('Done');
+  }, 10000);
+});
+
+await import('lume/cli.ts');
+
+console.log('Finished build...');
+
+console.log('Beginning closedown delay...');
+await new Promise((resolve) => {
+  setTimeout(() => {
+    console.log('Closedown delay completed...');
+    resolve('Done');
+  }, 10000);
+});
+
