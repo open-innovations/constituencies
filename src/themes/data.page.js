e5fda53f0fc237896164d9d53ac4366adecc3543
slowe <299787+slowe@users.noreply.github.com>
2025-01-16 20:56:45 +0000

    Generate index.json

--- src/themes/data.page.js
+++ src/themes/data.page.js
@@ -1,44 +1,69 @@
+import site from '../../_config.ts';
+
 export default function*({search}){
 	
 	const pages = search.pages("datafiles!=undefined");
+	let index = {};
 
 	for(const page of pages){
-		
+
 		const datafiles = page.datafiles;
 		const url = page.url;
+		const theme = page.theme;
 		for(const id of datafiles){
-			console.log(id,url+id+'.json');
+			console.log(theme,id,url+id+'.json');
+
+			const config = {...page[id].config};
 			const data = {};
 			
-			data.key = page[id].matchKey;
-			data.value = page[id].value;
-			data.data = page[page[id].data].rows;
+			data.key = config.matchKey;
+			data.value = config.value;
+			data.data = page[config.data].rows;
 
-			//if("tools" in page[id] && "slider" in page[id].tools) data.values = page[id].tools.slider.columns;
+			//if("tools" in config && "slider" in config.tools) data.values = config.tools.slider.columns;
 			
 			data.scale = {};
-			//if("scale" in page[id]) data.scale.type = page[id].scale;
-			if("min" in page[id]) data.scale.min = page[id].min;
-			if("max" in page[id]) data.scale.max = page[id].max;
+			//if("scale" in config) data.scale.type = config.scale;
+			if("min" in config) data.scale.min = config.min;
+			if("max" in config) data.scale.max = config.max;
 
-			if(page[id].hexjson=="hexjson.constituencies"){
+			if(config.hexjson=="hexjson.constituencies"){
 				data.hexjson = "https://open-innovations.org/projects/hexmaps/maps/constituencies.hexjson";
-				data.attribution = page[id].attribution + (page[id].attribution ? ' / ' : '') + '<a href="https://open-innovations.org/projects/hexmaps/hexjson">Hex layout</a>: <a href="https://open-innovations.org/projects/hexmaps/maps/constituencies.hexjson">2010 constituencies</a> (Open Innovations and contributors)';
-			}else if(page[id].hexjson=="hexjson.uk-constituencies-2024" || page[id].hexjson=="hexjson.uk-constituencies-2023-temporary"){
+				data.attribution = config.attribution + (config.attribution ? ' / ' : '') + '<a href="https://open-innovations.org/projects/hexmaps/hexjson">Hex layout</a>: <a href="https://open-innovations.org/projects/hexmaps/maps/constituencies.hexjson">2010 constituencies</a> (Open Innovations and contributors)';
+			}else if(config.hexjson=="hexjson.uk-constituencies-2024" || config.hexjson=="hexjson.uk-constituencies-2023-temporary"){
 				data.hexjson = "https://open-innovations.org/projects/hexmaps/maps/uk-constituencies-2023.hexjson";
-				data.attribution = page[id].attribution + (page[id].attribution ? ' / ' : '') + '<a href="https://open-innovations.org/projects/hexmaps/hexjson">Hex layout</a>: <a href="https://open-innovations.org/projects/hexmaps/maps/uk-constituencies-2023.hexjson">2024 constituencies</a> (Open Innovations and contributors)';
+				data.attribution = config.attribution + (config.attribution ? ' / ' : '') + '<a href="https://open-innovations.org/projects/hexmaps/hexjson">Hex layout</a>: <a href="https://open-innovations.org/projects/hexmaps/maps/uk-constituencies-2023.hexjson">2024 constituencies</a> (Open Innovations and contributors)';
 			}
-			data.legend = page[id].legend;
+			data.legend = config.legend;
 
-			// Format nicely
-			let content = JSON.stringify(data,null,"\t");
-			content = content.replace(/[\n\r]\t{2,}/g,'');
-			content = content.replace(/\n\t{1}([\}\]])(\,|\n)/g,function(m,p1,p2){ return p1+p2; });
+			// Add to themes
+			if(!(page.theme in index)){
+				index[page.theme] = {
+					'title':page.themes[page.theme].title,
+					'description':page.themes[page.theme].description,
+					'data':[]
+				};
+			}
+
+			const pageurl = url+id+'.json';
+
+			index[page.theme].data.push({'url':site.url(pageurl, true)});
 			
-			yield {'url':url+id+'.json','content':content};
+			yield {'url':pageurl,'content':niceJSON(data)};
 		}
-		
 	}
 
-	
+	yield {'url':'./index.json','content':niceJSON(index,3)};	
+}
+
+function escapeRegExp(string){
+    return string.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+}
+
+// Format JSON so that it is nicely indented at a particular depth
+function niceJSON(data,depth=1){
+	let content = JSON.stringify(data,null,"\t");
+	content = content.replace(new RegExp('[\n]\t{'+(depth+1)+',}', "g"),'');
+	content = content.replace(new RegExp('[\n]\t{'+(depth)+'}(\}|\])(\,|\n)', "g"),function(m,p1,p2){ return p1+p2; });
+	return content;
 }
