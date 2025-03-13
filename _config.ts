c12134be7864b63612ed05def7c703aa7936e259
slowe <299787+slowe@users.noreply.github.com>
2025-03-13 18:40:58 +0000

    Update off gas grid vis

--- _config.ts
+++ _config.ts
@@ -9,7 +9,7 @@ import sitemap from "lume/plugins/sitemap.ts";		// To build a site map
 import { expandGlobSync } from "lume/deps/fs.ts";
 
 // Importing the OI Lume charts and utilities
-import oiViz from "https://deno.land/x/oi_lume_viz@v0.16.9/mod.ts";
+import oiViz from "https://deno.land/x/oi_lume_viz@v0.16.10/mod.ts";
 import autoDependency from "https://deno.land/x/oi_lume_utils@v0.4.0/processors/auto-dependency.ts";
 import csvLoader from "https://deno.land/x/oi_lume_utils@v0.4.0/loaders/csv-loader.ts";
 import jsonLoader from "lume/core/loaders/json.ts";
